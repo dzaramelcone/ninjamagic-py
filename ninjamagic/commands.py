42815f4b7efd773eff3faf83b6b60d258cf7baac
Dzara Melcone <dzaramelcone@gmail.com>
2025-10-05 20:21:41 -0400

    add Attack to commands

--- ninjamagic/commands.py
+++ ninjamagic/commands.py
@@ -1,8 +1,11 @@
 import logging
 from typing import Protocol
 
+
 from ninjamagic import bus
-from ninjamagic.util import Compass, Reach
+from ninjamagic.component import name
+from ninjamagic.util import Compass, get_melee_delay
+from ninjamagic.visibility import find, visible, adjacent
 
 log = logging.getLogger(__name__)
 Out = tuple[bool, str]
@@ -30,7 +33,37 @@ class Move(Command):
         self.text = text
 
     def trigger(self, root: bus.Inbound) -> Out:
-        bus.pulse(bus.Move(source=root.source, dir=Compass(self.text)))
+        bus.pulse(bus.MoveCompass(source=root.source, dir=Compass(self.text)))
+        return OK
+
+
+class Attack(Command):
+    text: str = "attack"
+
+    def trigger(self, root: bus.Inbound) -> Out:
+        _, _, rest = root.text.partition(" ")
+        if not rest:
+            return False, "Attack whom?"
+        match = next(
+            find(root.source, rest, adjacent),
+            None,
+        )
+        if not match:
+            return False, "Attack whom?"
+        target, _, _ = match
+        bus.pulse(
+            bus.Act(
+                source=root.source,
+                delay=get_melee_delay(),
+                then=bus.Melee(source=root.source, target=target),
+            ),
+            bus.Outbound(to=root.source, text="You draw back your fist..."),
+            bus.Emit(
+                source=root.source,
+                reach=visible,
+                text=f"{name(root.source)} draws back their fist...",
+            ),
+        )
         return OK
 
 
@@ -46,13 +79,14 @@ class Say(Command):
         bus.pulse(bus.Outbound(to=root.source, text=first))
 
         second = f"They say, '{rest}'"
-        bus.pulse(bus.Emit(source=root.source, reach=Reach.adjacent, text=second))
+        bus.pulse(bus.Emit(source=root.source, reach=visible, text=second))
         return OK
 
 
 commands: list[Command] = [
+    *[Move(dir.value) for dir in Compass],
     *[Move(shortcut) for shortcut in ["ne", "se", "sw", "nw"]],
-    *[Move(str(dir)) for dir in list(Compass)],
     Look(),
     Say(),
+    Attack(),
 ]
