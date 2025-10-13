5c59ef0f31ee528617bfa13d5ef703a3f45761f1
Dzara Melcone <dzaramelcone@gmail.com>
2025-10-13 07:00:03 -0400

    conn uses factory

--- ninjamagic/conn.py
+++ ninjamagic/conn.py
@@ -2,9 +2,8 @@ import logging
 
 import esper
 
-from ninjamagic import bus
-from ninjamagic.world import demo_map, NOWHERE
-from ninjamagic.component import Connection, Transform, transform
+from ninjamagic import bus, factory
+from ninjamagic.component import Connection
 
 log = logging.getLogger(__name__)
 
@@ -13,33 +12,8 @@ def process():
     for c in bus.iter(bus.Connected):
         log.info("%s:%s connected.", c.client, c.source)
         esper.add_component(c.source, c.client, Connection)
-
-        # TODO: Factory to instantiate PC and NPC entities
-        esper.add_component(c.source, Transform(map_id=demo_map, x=1, y=1))
-        bus.pulse(
-            bus.PositionChanged(
-                source=c.source,
-                from_map_id=0,
-                from_x=0,
-                from_y=0,
-                to_map_id=demo_map,
-                to_x=1,
-                to_y=1,
-            )
-        )
+        factory.create(c.source)
 
     for d in bus.iter(bus.Disconnected):
         log.info("%s:%s disconnected.", d.client, d.source)
-        pos = transform(d.source)
-        bus.pulse(
-            bus.PositionChanged(
-                source=d.source,
-                from_map_id=pos.map_id,
-                from_x=pos.x,
-                from_y=pos.y,
-                to_map_id=NOWHERE,
-                to_x=1,
-                to_y=1,
-            )
-        )
-        esper.delete_entity(entity=d.source)
+        factory.destroy(c.source)
