d9b808788e2ab0ea42975d2d71181704859fc894
Dzara Melcone <dzaramelcone@gmail.com>
2025-11-28 19:05:16 -0500

    standup only if theyre resting

--- ninjamagic/regen.py
+++ ninjamagic/regen.py
@@ -11,11 +11,20 @@ next_call = 5
 def process(now: Walltime):
     global next_call
     if now >= next_call:
-        return
+        for eid, comps in esper.get_components(Health, Stance):
+            health, stance = comps
 
-    for eid, comps in esper.get_components(Health, Stance):
-        _, stance = comps
-        if stance.cur == "lying prone":
-            bus.pulse(bus.HealthChanged(source=eid, health_change=6, stress_change=-3))
+            if stance.cur == "lying prone":
+                bus.pulse(
+                    bus.HealthChanged(source=eid, health_change=6, stress_change=-3),
+                )
 
-    next_call = now + settings.regen_tick_rate
+                if (
+                    health.cur + 6 >= 100
+                    and health.stress - 3 <= health.aggravated_stress
+                ):
+                    bus.pulse(
+                        bus.StanceChanged(source=eid, stance="standing", echo=True)
+                    )
+
+        next_call = now + settings.regen_tick_rate
