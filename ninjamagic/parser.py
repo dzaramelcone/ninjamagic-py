f1ec86e6162717a383c603fb5df346c1864d37c0
Dzara Melcone <dzaramelcone@gmail.com>
2025-12-21 01:10:39 -0500

    stun causes continue

--- ninjamagic/parser.py
+++ ninjamagic/parser.py
@@ -25,6 +25,7 @@ def process():
         ok, err = commands.assert_not_stunned(sig.source)
         if not ok:
             bus.pulse(bus.Outbound(to=sig.source, text=err))
+            continue
 
         if match.requires_healthy:
             ok, err = commands.assert_healthy(sig.source)
