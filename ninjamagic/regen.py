dc2908e58352e26297b770c2baaa69a69615051c
Dzara Melcone <dzaramelcone@gmail.com>
2025-12-31 07:07:56 -0500

    sit rather than stand after full rest

--- ninjamagic/regen.py
+++ ninjamagic/regen.py
@@ -19,12 +19,9 @@ def process(now: Looptime):
                     bus.HealthChanged(source=eid, health_change=6, stress_change=-3),
                 )
 
-                if (
-                    health.cur + 6 >= 100
-                    and health.stress - 3 <= health.aggravated_stress
-                ):
+                if health.cur >= 100 and health.stress <= health.aggravated_stress:
                     bus.pulse(
-                        bus.StanceChanged(source=eid, stance="standing", echo=True)
+                        bus.StanceChanged(source=eid, stance="sitting", echo=True)
                     )
 
         next_call = now + settings.regen_tick_rate
