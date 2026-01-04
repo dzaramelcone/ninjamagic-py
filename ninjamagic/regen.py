414cc6c43528443fc56adf1cd7ea7861acdce3bc
Dzara Melcone <dzaramelcone@gmail.com>
2026-01-03 20:24:07 -0500

    regen carries over your prior stance when auto sitting up

--- ninjamagic/regen.py
+++ ninjamagic/regen.py
@@ -10,18 +10,20 @@ next_call = 5
 
 def process(now: Looptime):
     global next_call
-    if now >= next_call:
-        for eid, comps in esper.get_components(Health, Stance):
-            health, stance = comps
-
-            if stance.cur == "lying prone" and health.condition == "normal":
+    while now >= next_call:
+        for eid, (health, stance) in esper.get_components(Health, Stance):
+            if health.condition != "normal":
+                continue
+            if stance.cur == "lying prone":
                 bus.pulse(
                     bus.HealthChanged(source=eid, health_change=6, stress_change=-3),
                 )
-
                 if health.cur >= 100 and health.stress <= health.aggravated_stress:
+                    # Sit up.
                     bus.pulse(
-                        bus.StanceChanged(source=eid, stance="sitting", echo=True)
+                        bus.StanceChanged(
+                            source=eid, stance="sitting", prop=stance.prop, echo=True
+                        )
                     )
 
         next_call = now + settings.regen_tick_rate
