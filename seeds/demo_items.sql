7388e452fd3c8166221d81ff6d122bf56f9d8b61
Dzara Melcone <dzaramelcone@gmail.com>
2026-01-22 21:18:48 -0500

    add more demo/seed data

--- seeds/demo_items.sql
+++ seeds/demo_items.sql
@@ -2,9 +2,15 @@
 -- Note: DEMO is the second entity created after NOWHERE
 TRUNCATE inventories RESTART IDENTITY;
 INSERT INTO inventories
-    (eid, key,          slot, container_eid, map_id, x,    y,    level) VALUES
-    (1,   'lily_pad',   '',   NULL,          2,      8,    11,   0),
-    (2,   'backpack',   '',   NULL,          2,      9,    4,    0),
-    (3,   'cookpot',    '',   2,             NULL,   NULL, NULL, 0),
-    (4,   'bedroll',    '',   NULL,          2,      9,    4,    10),
-    (5,   'broadsword', '',   NULL,          2,      9,    4,    0);
+    (eid, key,          slot, container_eid, map_id, x,    y,    level, state) VALUES
+    -- Ground items
+    (1,   'lily_pad',   '',   NULL,          2,      8,    11,   0,     NULL),
+    (2,   'backpack',   '',   NULL,          2,      9,    4,    0,     NULL),
+    (3,   'bonfire',    '',   NULL,          2,      7,    7,    0,     NULL),
+    (4,   'bedroll',    '',   NULL,          2,      9,    4,    10,    NULL),
+    (5,   'broadsword', '',   NULL,          2,      9,    4,    0,     NULL),
+    -- Nested: cookpot in backpack
+    (6,   'cookpot',    '',   2,             NULL,   NULL, NULL, 0,     NULL),
+    -- Nested: leeks in cookpot (3 levels deep)
+    (7,   'leek',       '',   6,             NULL,   NULL, NULL, 0,     NULL),
+    (8,   'leek',       '',   6,             NULL,   NULL, NULL, 0,     '[{"kind": "Noun", "value": "chive"}]');
