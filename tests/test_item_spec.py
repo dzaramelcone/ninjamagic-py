418c26aa91c515ce37a8a4a8c4ddd539ba9ee6c7
Dzara Melcone <dzaramelcone@gmail.com>
2026-01-24 10:50:04 -0500

    add tests

--- tests/test_item_spec.py
+++ tests/test_item_spec.py
@@ -1,7 +1,7 @@
 import esper
 
 from ninjamagic.component import Transform
-from ninjamagic.inventory import create_item, dump_state
+from ninjamagic.inventory import create_item, State
 
 
 def test_serialize_state_returns_empty_json_when_unchanged():
@@ -9,5 +9,5 @@ def test_serialize_state_returns_empty_json_when_unchanged():
     eid = create_item("torch", transform=Transform(map_id=0, y=0, x=0), level=0)
 
     # Serialize state - should be empty JSON object since nothing changed
-    state = dump_state(eid, "torch")
+    state = State.from_entity(eid).model_dump_json()
     assert state == "{}"
